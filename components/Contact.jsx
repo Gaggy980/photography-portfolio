82930d56cee70b494fb01c01188ff64cd5d75e0d
Igor G <igorgagicit@gmail.com>
2022-12-14 15:49:27 +0000

    ...

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
   return (
     <div className="max-w-[1240px] m-auto p-4 h-screen">
       <h1 className="text-2hx font-bold text-center p-4">
-        Let's work Together
+        Let us work Together
       </h1>
       <form className="max-w-[600px] m-auto">
         <div className="grid grid-cols-2 gap-28">
